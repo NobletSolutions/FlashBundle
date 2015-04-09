2d24094f440ce6fb72cd14d0aed9e8a814602738
Nathanael d. Noblet <nathanael@noblet.ca>
2015-04-09 14:36:28 -0600

    php docblock and whitespace

--- Model/FlashMessage.php
+++ Model/FlashMessage.php
@@ -10,7 +10,14 @@ class FlashMessage implements Message
     private $title;
     private $message;
     private $buttonMessage;
-    
+
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     */
     public function __construct($header = null, $title = null, $message = null, $buttonMessage = null)
     {
         $this->header        = $header;
@@ -19,43 +26,83 @@ class FlashMessage implements Message
         $this->buttonMessage = (is_null($buttonMessage)) ? 'Close':$buttonMessage;
     }
 
+    /**
+     * @return string
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     *
+     * @param string $title
+     * @return \NS\FlashBundle\Model\FlashMessage
+     */
     public function setTitle($title)
     {
         $this->title = $title;
+
+        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getMessage()
     {
         return $this->message;
     }
 
+    /**
+     *
+     * @param string $message
+     * @return \NS\FlashBundle\Model\FlashMessage
+     */
     public function setMessage($message)
     {
         $this->message = $message;
+
+        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getButtonMessage()
     {
         return $this->buttonMessage;
     }
 
+    /**
+     *
+     * @param string $buttonMessage
+     * @return \NS\FlashBundle\Model\FlashMessage
+     */
     public function setButtonMessage($buttonMessage)
     {
         $this->buttonMessage = $buttonMessage;
+
+        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getHeader()
     {
         return $this->header;
     }
 
+    /**
+     *
+     * @param string $header
+     * @return \NS\FlashBundle\Model\FlashMessage
+     */
     public function setHeader($header)
     {
         $this->header = $header;
+
+        return $this;
     }
 }
--- Service/Messages.php
+++ Service/Messages.php
@@ -14,50 +14,103 @@ use NS\FlashBundle\Model\FlashMessage;
 class Messages extends \Twig_Extension implements MessageStore
 {
     private $session;
+
     private $flashBag;
+
     private $template;
+
     private $environment;
 
+    /**
+     *
+     * @param Session $session
+     * @param string $template
+     */
     public function __construct(Session $session, $template)
     {
         $this->session  = $session;
-        $this->flashBag = ($session->isStarted()) ? $session->getFlashBag():null;
+        $this->flashBag = ($session->isStarted()) ? $session->getFlashBag() : null;
         $this->template = $template;
     }
 
+    /**
+     *
+     * @param \Twig_Environment $environment
+     */
     public function initRuntime(\Twig_Environment $environment)
     {
         $this->environment = $environment;
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     * @return boolean
+     */
     public function addSuccess($header = null, $title = null, $message = null, $buttonMessage = null)
     {
         return $this->_add('success', $header, $title, $message, $buttonMessage);
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     * @return boolean
+     */
     public function addWarning($header = null, $title = null, $message = null, $buttonMessage = null)
     {
         return $this->_add('warning', $header, $title, $message, $buttonMessage);
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     * @return boolean
+     */
     public function addError($header = null, $title = null, $message = null, $buttonMessage = null)
     {
         return $this->_add('error', $header, $title, $message, $buttonMessage);
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     * @return boolean
+     */
     public function add($header = null, $title = null, $message = null, $buttonMessage = null)
     {
         return $this->_add('message', $header, $title, $message, $buttonMessage);
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $title
+     * @param string $message
+     * @param string $buttonMessage
+     * @return boolean
+     */
     private function _add($type, $header = null, $title = null, $message = null, $buttonMessage = null)
     {
-        if(!$this->flashBag && $this->session->isStarted())
-            $this->flashBag = $this->session->getFlashBag ();
+        if (!$this->flashBag && $this->session->isStarted()) {
+            $this->flashBag = $this->session->getFlashBag();
+        }
 
-        if($this->flashBag)
-        {
+        if ($this->flashBag) {
             $this->flashBag->add($type, new FlashMessage($header, $title, $message, $buttonMessage));
+
             return true;
         }
 
@@ -70,15 +123,23 @@ class Messages extends \Twig_Extension implements MessageStore
     public function getFunctions()
     {
         return array(
-            'flash_messages' => new \Twig_Function_Method($this, 'flash_messages',array('is_safe' => array('html')))
+            'flash_messages' => new \Twig_Function_Method($this, 'outputMessages', array(
+                'is_safe' => array('html')))
         );
     }
 
-    public function flash_messages()
+    /**
+     *
+     * @return string
+     */
+    public function outputMessages()
     {
-        return $this->environment->render($this->template, array('flashbag'=>$this->flashBag));
+        return $this->environment->render($this->template, array('flashbag' => $this->flashBag));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'flash_message';
