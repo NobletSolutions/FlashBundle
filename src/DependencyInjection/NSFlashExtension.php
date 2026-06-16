cf1cf9f76ae08124a8193ecad498fae125695c08
Mark Woynarski <143827+mwoynarski@users.noreply.github.com>
2026-06-16 11:36:07 -0600

    Upgrade to SF8

--- src/DependencyInjection/NSFlashExtension.php
+++ src/DependencyInjection/NSFlashExtension.php
@@ -4,12 +4,12 @@ namespace NS\FlashBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class NSFlashExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
